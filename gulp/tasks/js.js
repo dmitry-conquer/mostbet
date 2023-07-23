b42f4e221e559f63fb42e82b43869bc424c04e2f
Dmytro Frolov <dmitry.conquer@gmail.com>
2023-07-23 15:21:45 +0300

    init

---
+++ gulp/tasks/js.js
@@ -0,0 +1,26 @@
+import webpack from "webpack-stream";
+import ESLintPlugin from 'eslint-webpack-plugin';
+
+export const js = () => {
+  return app.gulp
+    .src(app.path.src.js, { sourcemaps: app.isDev })
+    .pipe(
+      webpack({
+        mode: app.isBuild ? "production" : "development",
+        output: {
+          filename: "app.min.js",
+        },
+        plugins: [
+          new ESLintPlugin({
+            emitError: true,
+            emitWarning: true,
+            failOnError: true,
+            failOnWarning: false,
+          }),
+        ],
+      })
+    )
+    .pipe(app.plugins.if(app.isBuild, app.plugins.size({ title: 'JS', showFiles: true })))
+    .pipe(app.gulp.dest(app.path.build.js))
+    .pipe(app.plugins.browserSync.stream());
+};
