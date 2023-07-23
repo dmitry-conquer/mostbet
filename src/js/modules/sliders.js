9c0540035fe2f8cd7e1faeef272f95b19acdcf72
Dmytro Frolov <dmitry.conquer@gmail.com>
2023-07-23 20:46:26 +0300

    update

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,62 +1,35 @@
 import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
 
-// Slider init function
 export function initSliders() {
-  //  Sliders list
   if (document.querySelector('.slider-hero')) {
     new Swiper('.slider-hero', {
-      // Сonnect modules (P.S dont forget !import scss styles)
       modules: [Pagination, Autoplay],
-
-      // Custom classes
       wrapperClass: 'slider-hero__wrapper',
       slideClass: 'slide-hero',
-
-      // Common settings
       slidesPerView: 1,
       spaceBetween: 0,
       speed: 1200,
       loop: true,
-      // centeredSlides: true,
-      // watchOverflow: false,
-      //autoHeight: true,
-      // watchSlidesProgress: true,
-
-      // Effects
       autoplay: {
         delay: 3000,
       },
-
-      // Pagination
       pagination: {
-         el: '.slider-hero__pagination',
-         clickable: true,
+        el: '.slider-hero__pagination',
+        clickable: true,
       },
-
-      // Events
-      /*
-         on: {
-            // - - - - - - - [custom fraction] - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-            init: function (swiper) {
-              const allSliders = document.querySelector('.slider__all');
-              allSliders.innerHTML = swiper.slides.length;
-              console.log(allSliders.innerHTML);
-            },
-            slideChange: function (swiper) {
-              const currentSlide = document.querySelector('.slider__current');
-              currentSlide.innerHTML = swiper.realIndex + 1;
-              console.log(currentSlide.innerHTML);
-            },
-         },
-            */
     });
+  }
 
-    // stop slider on mouseenter
-    // defaultSlider.el.addEventListener('mouseenter', () => {
-    //    defaultSlider.autoplay.stop();
-    // });
-    // defaultSlider.el.addEventListener('mouseleave', () => {
-    //    defaultSlider.autoplay.start();
-    // });
+  if (document.querySelector('.slider-games')) {
+    new Swiper('.slider-games', {
+      modules: [Navigation],
+      wrapperClass: 'slider-games__wrapper',
+      slideClass: 'slider-games__slide',
+      slidesPerView: 6,
+      spaceBetween: 20,
+      speed: 1200,
+      // loop: true,
+      navigation: {},
+    });
   }
 }
