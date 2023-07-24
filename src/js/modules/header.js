5f0d48822a64b45bf926a4f3a863d2f070f18b81
Dmytro Frolov <dmitry.conquer@gmail.com>
2023-07-24 07:35:30 +0300

    header update dynamic adapt

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,8 +1,11 @@
 export function initHeader() {
-  const headerIcon = document.querySelector('.header__menu-icon');
+  const headerOpenButton = document.querySelector('.header__menu-icon');
+  const headerCloseButton = document.querySelector('.nav-header__close');
   const headerNavigation = document.querySelector('.nav-header');
-  headerIcon.addEventListener('click', () => {
-   console.log('sdsad');
+  headerOpenButton.addEventListener('click', () => {
     headerNavigation.classList.toggle('_active');
+  });
+  headerCloseButton.addEventListener('click', () => {
+    headerNavigation.classList.remove('_active');
   });
 }
