f25d00a8d90023e4337b84ade04757fb45fffe69
Dmytro Frolov <dmitry.conquer@gmail.com>
2023-07-24 10:51:17 +0300

    /single.html done

--- src/js/app.js
+++ src/js/app.js
@@ -29,14 +29,18 @@ function app() {
   const headerOpenButton = document.querySelector('.header__menu-icon');
   const headerCloseButton = document.querySelector('.nav-header__close');
   const headerNavigation = document.querySelector('.nav-header');
-  headerOpenButton.addEventListener('click', () => {
-    headerNavigation.classList.toggle('_active');
-    document.body.classList.toggle('_lock')
-  });
-  headerCloseButton.addEventListener('click', () => {
-    headerNavigation.classList.remove('_active');
-    document.body.classList.remove('_lock')
-  });
+  if (headerOpenButton) {
+    headerOpenButton.addEventListener('click', () => {
+      headerNavigation.classList.toggle('_active');
+      document.body.classList.toggle('_lock');
+    });
+  }
+  if (headerCloseButton) {
+    headerCloseButton.addEventListener('click', () => {
+      headerNavigation.classList.remove('_active');
+      document.body.classList.remove('_lock');
+    });
+  }
 
   /*
     sidebar
--- src/js/modules/sorting.js
+++ src/js/modules/sorting.js
@@ -1,8 +1,10 @@
 export function sorting() {
-   const sortTrigger = document.querySelector('.sort__trigger');
-   const dropDown = sortTrigger.nextElementSibling;
-   sortTrigger.addEventListener('click', () => {
-      dropDown.classList.toggle('_visible')
-      sortTrigger.classList.toggle('_active')
-   })
+  const sortTrigger = document.querySelector('.sort__trigger');
+  if (sortTrigger) {
+    const dropDown = sortTrigger.nextElementSibling;
+    sortTrigger.addEventListener('click', () => {
+      dropDown.classList.toggle('_visible');
+      sortTrigger.classList.toggle('_active');
+    });
+  }
 }
